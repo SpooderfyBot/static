af65baa4b9a9552d006b4d1fbbed5a0b59ed0d1a
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-12-04 18:27:50 +0000

    update and add stuff

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use warp::Filter;
+use std::{error, fs};
+use std::net::SocketAddr;
+use serde::{Deserialize, Serialize};
+use chrono::Utc;
+
+use mimalloc::MiMalloc;
+
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
+
+#[tokio::main]
+async fn main() {
+    let config = get_config();
+
+    let home = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file(&config.home_dir));
+
+    let home2 = warp::path("home")
+        .and(warp::fs::file(&config.home_dir));
+
+    let static_files = warp::path("static")
+        .and(warp::fs::dir(&config.static_file_dir));
+
+    println!(
+        "[ {} ] Running @ ws://{}",
+        Utc::now().format("%D | %T"),
+        &config.server_host
+    );
+
+    let server: SocketAddr = config.server_host
+        .parse()
+        .expect("Unable to parse socket address");
+
+    let routes = home.or(home2).or(static_files);
+
+    warp::serve(routes).run(server).await;
+}
+
+fn get_config() -> ServerConfig {
+    let config = fs::read_to_string("./config.json")
+        .expect("could not load config");
+    let config: ServerConfig = serde_json::from_str(&config)
+        .expect("could not parse json");
+
+
+    config
+}
+
+#[derive(Serialize, Deserialize)]
+struct ServerConfig {
+    server_host: String,
+    static_file_dir: String,
+    home_dir: String,
+}
