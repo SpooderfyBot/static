4b5017368576cabcf0f704971125fd00f8b51de9
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2020-12-14 17:13:19 +0000

    update stuff

--- static/js/player.js
+++ static/js/player.js
@@ -1,3 +1,4 @@
+// Select elements here
 const video = document.getElementById('video');
 const videoControls = document.getElementById('video-controls');
 const playButton = document.getElementById('play');
@@ -21,208 +22,207 @@ const pipButton = document.getElementById('pip-button')
 
 const videoWorks = !!document.createElement('video').canPlayType;
 if (videoWorks) {
-    video.controls = false
-    videoControls.classList.remove('hidden');
+  video.controls = false
+  videoControls.classList.remove('hidden');
 }
 
 function togglePlay() {
-    if (video.paused || video.ended) {
-        video.play();
-    } else {
-        video.pause();
-    }
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 function updatePlayButton() {
-    playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+  playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
 
-    if (video.paused) {
-        playButton.setAttribute('data-title', 'Play (k)')
-    } else {
-        playButton.setAttribute('data-title', 'Pause (k)')
-    }
+  if (video.paused) {
+    playButton.setAttribute('data-title', 'Play (k)')
+  } else {
+    playButton.setAttribute('data-title', 'Pause (k)')
+  }
 }
 
 function formatTime(timeInSeconds) {
-    const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+  const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
 
-    return {
-        minutes: result.substr(3, 2),
-        seconds: result.substr(6, 2),
-    };
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
 }
 
 function initializeVideo() {
-    const videoDuration = Math.round(video.duration);
-    seek.setAttribute('max', videoDuration);
-    progressBar.setAttribute('max', videoDuration);
-    const time = formatTime(videoDuration);
-    duration.innerText = `${time.minutes}:${time.seconds}`;
-    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  const videoDuration = Math.round(video.duration);
+  seek.setAttribute('max', videoDuration);
+  progressBar.setAttribute('max', videoDuration);
+  const time = formatTime(videoDuration);
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
 }
 
 function updateTimeElapsed() {
-    const time = formatTime(Math.round(video.currentTime));
-    timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
-    timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  const time = formatTime(Math.round(video.currentTime));
+  timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
 }
 
 function updateProgress() {
-    seek.value = Math.floor(video.currentTime);
-    progressBar.value = Math.floor(video.currentTime);
+  seek.value = Math.floor(video.currentTime);
+  progressBar.value = Math.floor(video.currentTime);
 }
 
 function updateSeekTooltip(event) {
-    const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
-    seek.setAttribute('data-seek', skipTo)
-    const t = formatTime(skipTo);
-    seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
-    const rect = video.getBoundingClientRect();
-    seekTooltip.style.left = `${event.pageX - rect.left}px`;
+  const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
+  seek.setAttribute('data-seek', skipTo)
+  const t = formatTime(skipTo);
+  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+  const rect = video.getBoundingClientRect();
+  seekTooltip.style.left = `${event.pageX - rect.left}px`;
 }
 
 function skipAhead(event) {
-    const skipTo = event.target.dataset.seek
-        ? event.target.dataset.seek
-        : event.target.value;
-    video.currentTime = skipTo;
-    progressBar.value = skipTo;
-    seek.value = skipTo;
+  const skipTo = event.target.dataset.seek
+    ? event.target.dataset.seek
+    : event.target.value;
+  video.currentTime = skipTo;
+  progressBar.value = skipTo;
+  seek.value = skipTo;
 }
 
 function updateVolume() {
-    if (video.muted) {
-        video.muted = false;
-    }
+  if (video.muted) {
+    video.muted = false;
+  }
 
-    video.volume = volume.value;
+  video.volume = volume.value;
 }
 
 function updateVolumeIcon() {
-    volumeIcons.forEach(icon => {
-        icon.classList.add('hidden');
-    });
+  volumeIcons.forEach(icon => {
+    icon.classList.add('hidden');
+  });
 
-    volumeButton.setAttribute('data-title', 'Mute (m)')
+  volumeButton.setAttribute('data-title', 'Mute (m)')
 
-    if (video.muted || video.volume === 0) {
-        volumeMute.classList.remove('hidden');
-        volumeButton.setAttribute('data-title', 'Unmute (m)')
-    } else if (video.volume > 0 && video.volume <= 0.5) {
-        volumeLow.classList.remove('hidden');
-    } else {
-        volumeHigh.classList.remove('hidden');
-    }
+  if (video.muted || video.volume === 0) {
+    volumeMute.classList.remove('hidden');
+    volumeButton.setAttribute('data-title', 'Unmute (m)')
+  } else if (video.volume > 0 && video.volume <= 0.5) {
+    volumeLow.classList.remove('hidden');
+  } else {
+    volumeHigh.classList.remove('hidden');
+  }
 }
 
 function toggleMute() {
-    video.muted = !video.muted;
+  video.muted = !video.muted;
 
-    if (video.muted) {
-        volume.setAttribute('data-volume', volume.value);
-        volume.value = 0;
-    } else {
-        volume.value = volume.dataset.volume;
-    }
+  if (video.muted) {
+    volume.setAttribute('data-volume', volume.value);
+    volume.value = 0;
+  } else {
+    volume.value = volume.dataset.volume;
+  }
 }
 
 function animatePlayback() {
-    playbackAnimation.animate([
-        {
-            opacity: 1,
-            transform: "scale(1)",
-        },
-        {
-            opacity: 0,
-            transform: "scale(1.3)",
-        }
-    ], {
-        duration: 500,
-    });
+  playbackAnimation.animate([
+    {
+      opacity: 1,
+      transform: "scale(1)",
+    },
+    {
+      opacity: 0,
+      transform: "scale(1.3)",
+    }
+  ], {
+    duration: 500,
+  });
 }
 
 function toggleFullScreen() {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-    } else {
-        videoContainer.requestFullscreen();
-    }
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    videoContainer.requestFullscreen();
+  }
 }
 
 function updateFullscreenButton() {
-    fullscreenIcons.forEach(icon => icon.classList.toggle('hidden'));
+  fullscreenIcons.forEach(icon => icon.classList.toggle('hidden'));
 
-    if (document.fullscreenElement) {
-        fullscreenButton.setAttribute('data-title', 'Exit full screen (f)')
-    } else {
-        fullscreenButton.setAttribute('data-title', 'Full screen (f)')
-    }
+  if (document.fullscreenElement) {
+    fullscreenButton.setAttribute('data-title', 'Exit full screen (f)')
+  } else {
+    fullscreenButton.setAttribute('data-title', 'Full screen (f)')
+  }
 }
 
 async function togglePip() {
-    try {
-        if (video !== document.pictureInPictureElement) {
-            pipButton.disabled = true;
-            await video.requestPictureInPicture();
-        } else {
-            await document.exitPictureInPicture();
-        }
-    } catch (error) {
-        console.error(error)
-    } finally {
-        pipButton.disabled = false;
+  try {
+    if (video !== document.pictureInPictureElement) {
+      pipButton.disabled = true;
+      await video.requestPictureInPicture();
+    } else {
+      await document.exitPictureInPicture();
     }
+  } catch (error) {
+    console.error(error)
+  } finally {
+    pipButton.disabled = false;
+  }
 }
 
 let stillValid = true;
-
 function hideControls() {
-    if (video.paused || !stillValid) {
-        return;
-    }
+  if (video.paused || !stillValid)  {
+    return;
+  }
 
-    videoControls.classList.add('hide');
+  videoControls.classList.add('hide');
 }
 
 // showControls displays the video controls
 function showControls() {
-    stillValid = false;
-    videoControls.classList.remove('hide');
+  stillValid = false;
+  videoControls.classList.remove('hide');
 }
 
 function showControlsWithTimeout() {
-    showControls();
-    stillValid = true;
-    setTimeout(hideControls, 3000)
+  showControls();
+  stillValid = true;
+  setTimeout(hideControls, 3000)
 }
 
 
 // keyboardShortcuts executes the relevant functions for
 // each supported shortcut key
 function keyboardShortcuts(event) {
-    const {key} = event;
-    switch (key) {
-        case 'k':
-            togglePlay();
-            animatePlayback();
-            if (video.paused) {
-                showControls();
-            } else {
-                setTimeout(() => {
-                    hideControls();
-                }, 2000);
-            }
-            break;
-        case 'm':
-            toggleMute();
-            break;
-        case 'f':
-            toggleFullScreen();
-            break;
-        case 'p':
-            togglePip();
-            break;
-    }
+  const { key } = event;
+  switch(key) {
+    case 'k':
+      togglePlay();
+      animatePlayback();
+      if (video.paused) {
+        showControls();
+      } else {
+        setTimeout(() => {
+          hideControls();
+        }, 2000);
+      }
+      break;
+    case 'm':
+      toggleMute();
+      break;
+    case 'f':
+      toggleFullScreen();
+      break;
+    case 'p':
+      togglePip();
+      break;
+  }
 }
 
 
@@ -254,9 +254,8 @@ videoContainer.addEventListener('fullscreenchange', updateFullscreenButton);
 pipButton.addEventListener('click', togglePip);
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (!('pictureInPictureEnabled' in document)) {
-        pipButton.classList.add('hidden');
-    }
+  if (!('pictureInPictureEnabled' in document)) {
+    pipButton.classList.add('hidden');
+  }
 });
 document.addEventListener('keyup', keyboardShortcuts);
-
